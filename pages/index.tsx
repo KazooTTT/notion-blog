e61ccaa73fb6a2623c647593904cc7678c1abd6d
KazooTTT <yjwang.echo@gmail.com>
2023-09-13 23:50:31 +0800

    refactor:移除首页的评论

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import * as React from 'react'
 import { NotionPage } from '@/components/NotionPage'
 import { domain } from '@/lib/config'
 import { resolveNotionPage } from '@/lib/resolve-notion-page'
-import Comment from '@/components/Comment'
 
 export const getStaticProps = async () => {
   try {
