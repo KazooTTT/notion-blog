de0309afc2df7c7c3c7e27bb729804d070358e61
KazooTTT <yjwang.echo@gmail.com>
2023-09-13 23:51:30 +0800

    feat:giscus适配优化

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,12 +1,24 @@
 import { giscusProps } from '@/lib/config'
 import Giscus from '@giscus/react'
 import React from 'react'
-import styles from './styles.module.css'
+import styles from './giscus.module.css'
+import { useDarkMode } from '@/lib/use-dark-mode'
+import { cs } from 'react-notion-x'
 
 export default function Comment() {
+  const { isDarkMode } = useDarkMode()
   return (
-    <div className={styles['giscus-comments']}>
-      <Giscus {...giscusProps} />
+    <div
+      className={cs('comment-container', styles['giscus-comments-container'])}
+    >
+      <div className={styles['giscus-comments']}>
+        <Giscus
+          {...{
+            ...giscusProps,
+            theme: isDarkMode ? 'dark' : 'light'
+          }}
+        />
+      </div>
     </div>
   )
 }
