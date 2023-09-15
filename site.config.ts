473a79b0ad52ab67d10b94fef4e1e5f6fde08ef5
kazoottt <yjwang.echo@gmail.com>
2023-09-15 12:02:38 +0800

    feat:使用自定义顶部栏，去除底部的主题切换

--- site.config.ts
+++ site.config.ts
@@ -51,18 +51,18 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default',
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default',
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '54de916b2a7e46ca9d91ab8d38087245'
+    },
+    {
+      title: 'Contact',
+      pageId: 'fad97689ee4144e78613f86e3367779e'
+    }
+  ],
 
   giscusProps: {
     repo: 'kazoottt/notion-blog',
