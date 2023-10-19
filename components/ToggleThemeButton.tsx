86bbf96a9d00d1ec6e3ec109a007ff661fdfd192
kazoottt <yjwang.echo@gmail.com>
2023-10-19 16:42:58 +0800

    refactor: move `ToggleThemeButton` to single file

---
+++ components/ToggleThemeButton.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { useDarkMode } from '@/lib/use-dark-mode'
+import cs from 'classnames'
+import styles from '@/components/styles.module.css'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+
+export const ToggleThemeButton = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  const onToggleTheme = React.useCallback(() => {
+    toggleDarkMode()
+  }, [toggleDarkMode])
+
+  return (
+    <div
+      className={cs('breadcrumb', 'button', !hasMounted && styles.hidden)}
+      onClick={onToggleTheme}
+    >
+      {hasMounted && isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+    </div>
+  )
+}
