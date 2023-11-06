936d4ca973c80bfa8d00ca74615696354af9dc46
kazoottt <yjwang.echo@gmail.com>
2023-11-06 17:19:52 +0800

    fix: wrong github profile href

--- components/GithubCard.tsx
+++ components/GithubCard.tsx
@@ -4,18 +4,21 @@ import { GitHubIcon } from './GithubIcon'
 import Link from 'next/link'
 
 export const GithubCard = () => {
-  return (
+  const githubUserName = getSiteConfig('github') as string
+  return githubUserName ? (
     <Link
       className='flex flex-col p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 justify-center items-center'
-      href={`https://github.com/`}
+      href={`https://github.com/${githubUserName}`}
     >
       <GitHubIcon className='h-8 w-8 mb-2' />
       <GitHubCalendar
-        username={getSiteConfig('github')}
+        username={githubUserName}
         weekStart={1}
         hideColorLegend
         hideTotalCount
       />
     </Link>
+  ) : (
+    <></>
   )
 }
