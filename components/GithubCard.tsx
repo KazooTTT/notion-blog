b5798c8b89f40b023f8a2991ac269c356d5fc57a
KazooTTT <yjwang.echo@gmail.com>
2023-11-06 23:58:21 +0800

    fix: github card wrong theme config

--- components/GithubCard.tsx
+++ components/GithubCard.tsx
@@ -2,12 +2,19 @@ import { getSiteConfig } from '@/lib/get-config-value'
 import GitHubCalendar from 'react-github-calendar'
 import { GitHubIcon } from './GithubIcon'
 import Link from 'next/link'
+import { useDarkMode } from '@/lib/use-dark-mode'
+import { cn } from '@/lib/classNames'
 
 export const GithubCard = () => {
   const githubUserName = getSiteConfig('github') as string
+  const { isDarkMode } = useDarkMode()
+
   return githubUserName ? (
     <Link
-      className='flex flex-col p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 justify-center items-center'
+      className={cn(
+        `flex flex-col p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 justify-center items-center`,
+        isDarkMode && `bg-gray-800 border-gray-700 hover:bg-gray-700`
+      )}
       href={`https://github.com/${githubUserName}`}
     >
       <GitHubIcon className='h-8 w-8 mb-2' />
@@ -16,6 +23,7 @@ export const GithubCard = () => {
         weekStart={1}
         hideColorLegend
         hideTotalCount
+        colorScheme={isDarkMode ? 'dark' : 'light'}
       />
     </Link>
   ) : (
